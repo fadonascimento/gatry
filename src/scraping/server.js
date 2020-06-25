d7b3de895fe9eb69ea9bf6505e32690129b2faa2
Caio Fabiano <caiofab@gmail.com>
2020-06-24 22:10:33 -0300

    netlify server

--- src/scraping/server.js
+++ src/scraping/server.js
@@ -1,21 +1,24 @@
 const {GatryScraping} = require('./modules/scraping/scraping');
 const express = require('express');
+const serverless = require('serverless-http');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const router = express.Router();
 
-app.get('/api/results', async (req, res) => {
+router.get('/api/results', async (req, res) => {
   console.log('------- WEB SERVICE');
   const scrap = new GatryScraping();
   const response = await scrap.getResults();
   res.send(response);
 });
 
-app.get('/api/check-updates/:timestamp', async (req, res) => {
+router.get('/api/check-updates/:timestamp', async (req, res) => {
   const timestamp = req.params.timestamp;
   const scrap = new GatryScraping();
   const response = await scrap.getUpdates();
   res.send(response);
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use('/.netlify/functions/server', router);
+
+module.exports.handler = serverless(app);
