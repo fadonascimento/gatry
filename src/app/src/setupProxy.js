57ecf451f267c9d7b9a586fe0ca7cc6bff445414
Caio Fabiano <caiofab@gmail.com>
2020-06-24 21:18:27 -0300

    changing proxy settings

--- src/app/src/setupProxy.js
+++ src/app/src/setupProxy.js
@@ -1,8 +1,9 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const proxy = require('http-proxy-middleware');
+const port = process.env.PORT || 5000;
 
 module.exports = function(app) {
   app.use(createProxyMiddleware('/api/',
-    { target: 'http://localhost:5000' }
+    { target: `http://localhost:${port}` }
   ));
 }
